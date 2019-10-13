e693332787582b43e30845dd8cd77d52c496c52c
petr-konovalov <petrkon98@mail.ru>
2019-10-13 17:37:04 +0300

    Update RotateToPD

--- RotateToPD.m
+++ RotateToPD.m
@@ -1,18 +1,38 @@
 function [rul] = RotateToPD(agent, aimPoint, minAngularSpeed, P, D, eps)    
     function [angSpeed] = PIDAngFunction(angDiff)
         persistent oldAngDiff;
+        persistent oldT;
+        persistent oldAngSpeed;
         
-        if (isempty(oldAngDiff))
+        resetTime = 0.5;
+        minReactionTime = 0.05;
+        if isempty(oldT)
+            oldT = zeros(1, 12);
+        end
+        curT = cputime();
+        dT = curT - oldT(agent.id);
+        
+        if isempty(oldAngDiff) || dT > resetTime
             oldAngDiff = zeros(1, 12);
         end
         
-        if abs(angDiff) > eps
-            angSpeed = sign(angDiff) * minAngularSpeed + angDiff * P + (oldAngDiff(agent.id) - angDiff) * D;
-        else
-            angSpeed = 0;
+        if isempty(oldAngSpeed) || dT > resetTime
+            oldAngSpeed = zeros(1, 12);
         end
         
-        oldAngDiff(agent.id) = angDiff;
+        if dT < minReactionTime
+            angSpeed = oldAngSpeed(agent.id);
+        else
+            if abs(angDiff) > eps
+                angSpeed = sign(angDiff) * minAngularSpeed + angDiff * P + (oldAngDiff(agent.id) - angDiff) * D / dT;
+            else
+                angSpeed = 0;
+            end
+
+            oldAngDiff(agent.id) = angDiff;
+            oldT(agent.id) = curT;
+            oldAngSpeed(agent.id) = angSpeed;
+        end
     end
 
     rul = Crul(0, 0, 0, RotateTo(agent, aimPoint, @PIDAngFunction), 0);
