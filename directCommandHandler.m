53651cf3f4aafce866bce0054dab5f1422b82479
petrk <petrkon98@gmail.com>
2021-06-22 00:10:26 +0300

    Direct command handler fix

--- directCommandHandler.m
+++ directCommandHandler.m
@@ -1,6 +1,6 @@
 function ruls = directCommandHandler(sd, ks, coms, ball, BPosHX, BPosHY, BState, Gs, Vs, obstacles, goalSizes)
 	if sd == ks
-		ruls = freeKick(coms(sd, :), ball, getKickPoint(sd, ball, coms, Gs), BPosHX, BPosHY, obstacles);
+		ruls = freeKick(coms(sd, :), ball, getKickPoint(sd, ball, coms, Gs(3-sd, :)), BPosHX, BPosHY, obstacles);
 	else
 		keeperId = size(coms, 2);
 		ruls = passiveDirectBahaviour(sd, coms(:, 1:keeperId-1), ball, BState, Gs, Vs, obstacles, goalSizes);
@@ -12,13 +12,14 @@ function ruls = passiveDirectBahaviour(sd, coms, ball, BState, Gs, Vs, obstacles
 	ruls = stopCommandHandler(sd, coms, ball, BState, Gs, Vs, obstacles, goalSizes);
 end
 
-function pnt = getKickPoint(sd, ball, coms, Gs)
+function pnt = getKickPoint(sd, ball, coms, G)
 	ballFiltDist = 1500;
 	minDist = 1000000;
 	pnt = [0, 0];
-	for k = 1: size(coms, 2)
-		curDist = norm(coms(sd, k).z-Gs(3-sd, :))
+	for k = 1: size(coms, 2)-1
+		curDist = norm(coms(sd, k).z-G);
 		if curDist < minDist && norm(coms(sd, k).z-ball.z) > ballFiltDist
+			minDist = curDist;
 			pnt = coms(sd, k).z;
 		end
  	end
