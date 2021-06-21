1258199e0279278a13e8dbb9b33415aad41e2c0e
petrk <petrkon98@gmail.com>
2021-06-21 23:54:01 +0300

    Direct command handler fix

--- directCommandHandler.m
+++ directCommandHandler.m
@@ -1,6 +1,6 @@
 function ruls = directCommandHandler(sd, ks, coms, ball, BPosHX, BPosHY, BState, Gs, Vs, obstacles, goalSizes)
 	if sd == ks
-		ruls = freeKick(coms(sd, :), ball, getKickPoint(), BPosHX, BPosHY, obstacles);
+		ruls = freeKick(coms(sd, :), ball, getKickPoint(sd, ball, coms, Gs), BPosHX, BPosHY, obstacles);
 	else
 		keeperId = size(coms, 2);
 		ruls = passiveDirectBahaviour(sd, coms(:, 1:keeperId-1), ball, BState, Gs, Vs, obstacles, goalSizes);
@@ -12,7 +12,15 @@ function ruls = passiveDirectBahaviour(sd, coms, ball, BState, Gs, Vs, obstacles
 	ruls = stopCommandHandler(sd, coms, ball, BState, Gs, Vs, obstacles, goalSizes);
 end
 
-function pnt = getKickPoint()
+function pnt = getKickPoint(sd, ball, coms, Gs)
+	ballFiltDist = 1500;
+	minDist = 1000000;
 	pnt = [0, 0];
+	for k = 1: size(coms, 2)
+		curDist = norm(coms(sd, k).z-Gs(3-sd, k))
+		if curDist < minDist && norm(coms(sd, k).z-ball.z) > ballFiltDist
+			pnt = coms(sd, k).z;
+		end
+ 	end
 end
 
