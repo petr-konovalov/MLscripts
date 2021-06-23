b4c13a29be1dae71f03083792f5e199c177a8516
petrk <petrkon98@gmail.com>
2021-06-23 14:45:38 +0300

    getAttacker fix

--- getAttacker.m
+++ getAttacker.m
@@ -1,7 +1,13 @@
 function res = getAttacker(com, ball)
 	res = 1;
-	for k = 2: size(com, 2) - 1
-		if (r_dist_points(com(res).z, ball.z) > r_dist_points(com(k).z, ball.z))
+	for k = 1: size(com, 2)
+		if com(k).I ~= 0
+			res = k;
+			break;
+		end
+	end
+	for k = 1: size(com, 2) - 1
+		if (com(k).I ~= 0 && r_dist_points(com(res).z, ball.z) > r_dist_points(com(k).z, ball.z))
 		    res = k;
 		end
 	end
