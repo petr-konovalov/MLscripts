6f54e234f9df9f8c7ffdf8262760da134c8877de
petrk <petrkon98@gmail.com>
2021-06-23 20:06:28 +0300

    switch sides

--- kickoffCommandHandler.m
+++ kickoffCommandHandler.m
@@ -5,7 +5,7 @@ function ruls = kickoffCommandHandler(sd, coms, obstacles, kickoffTeam, ball, go
 	if sd == kickoffTeam
 		%active start
 		%ruls = gameModel(sd, coms, obstacles, ball, goals, Vs, field, BState, BPosHX, BPosHY, 1);
-		ruls = freeKick(coms(sd, :), ball, getKickPoint(sd, ball, coms, goals(3-sd, :), Vs(3-sd, :)), BPosHX, BPosHY, obstacles);
+		ruls = freeKick(coms(sd, :), ball, goals(3-sd, :), BPosHX, BPosHY, obstacles);
 	else
 		%passive start
 		ruls = gameModel(sd, coms, obstacles, ball, goals, Vs, field, BState, BPosHX, BPosHY, 2);
