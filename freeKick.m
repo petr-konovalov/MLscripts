64b6bb4abca9fdeb72864f57c50d3c2d624c81a0
petrk <petrkon98@gmail.com>
2021-06-23 15:11:49 +0300

    f

--- freeKick.m
+++ freeKick.m
@@ -13,7 +13,6 @@ function ruls = freeKick(com, ball, kickPoint, BPosHX, BPosHY, obstacles, kickTy
 	ball = getFilteredBall(ball, BPosHX, BPosHY);
 	attackerId = getAttacker(com, ball);
 	attacker = com(attackerId);
-	disp(attacker.id);
 	if isempty(state) || norm(ball.z-attacker.z) > 1000 || curTime - oldTime > 1
 		state = 0;
 	end
